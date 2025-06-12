3daa35382c4549664d031ffd96e6219b44a9d95a
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-12 15:21:32 -0600

    add factory function for creating elements and create a container div with it

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hi");
+const body = document.querySelector("body");
+
+function elementFactory(typeOfElement, className) {
+  const element = document.createElement(`${typeOfElement}`);
+  element.classList.add(className);
+
+  body.append(element);
+}
+
+elementFactory("div", "container");
