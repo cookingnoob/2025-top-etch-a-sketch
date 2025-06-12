03bef787b3683339ad108dd92616bf0d0c3b52ce
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-12 15:35:44 -0600

    create empty array to loop through and create the cell elements

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const body = document.querySelector("body");
 function elementFactory(typeOfElement, className) {
   const element = document.createElement(`${typeOfElement}`);
   element.classList.add(className);
-
   body.append(element);
   return element;
 }
 
 function createBoard() {
   const container = elementFactory("div", "container");
-  container.textContent = "hola hola";
-  console.log(container);
+  Array.apply(null, Array(256)).map((c, index) => {
+    const cell = elementFactory("div", "cell");
+    container.appendChild(cell);
+  });
 }
+
 createBoard();
