8e21f2f652f91465fdaa2ab23885e40bee074921
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-06-13 08:31:10 -0600

    add containers for the number and color buttons, refactor boardButtons function to handle numbers and colors buttons, add new parameters to boardButtons to work with new function arguments

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const body = document.querySelector("body");
 
+const buttonContainer = elementFactory("div", "button-container");
+const numberButtonContainer = elementFactory("div", "btn-number-container");
+const colorButtonContainer = elementFactory("div", "btn-color-container");
+buttonContainer.append(colorButtonContainer, numberButtonContainer);
+
+body.appendChild(buttonContainer);
+
 function elementFactory(typeOfElement, className) {
   const element = document.createElement(`${typeOfElement}`);
   element.classList.add(className);
@@ -18,10 +25,37 @@ function createBoard(rows, cols) {
   });
 }
 
-createBoard(101, 101);
-
 function handleHover(e, color1 = "white", color2 = "black") {
   const button = e.target;
   button.style.backgroundColor =
     button.style.backgroundColor === color2 ? color1 : color2;
 }
+
+function boardButtons(num, textColor, className, handler) {
+  const button = elementFactory("button", className);
+  if (num) {
+    button.textContent = `${num}x${num}`;
+  } else if (textColor) {
+    button.textContent = `${textColor}`;
+  }
+  button.value = num;
+  button.addEventListener("click", handleNewBoard);
+  if (num) {
+    numberButtonContainer.appendChild(button);
+    return;
+  } else {
+    colorButtonContainer.appendChild(button);
+  }
+}
+
+function handleNewBoard(e) {
+  const oldBoard = document.querySelector(".container");
+  oldBoard.remove();
+  createBoard(+e.target.value, +e.target.value);
+}
+
+boardButtons(16, "", "button-board", handleNewBoard);
+boardButtons(64, "", "button-board", handleNewBoard);
+boardButtons(100, "", "button-board", handleNewBoard);
+boardButtons(0, "50 Shades of Gray", "button-board", handleNewBoard);
+createBoard(16, 16);
